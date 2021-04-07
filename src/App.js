24da53283875af7d7ccdd18153a8997dacaa18f0
Chase Lones <chaselones@gmail.com>
2021-04-07 11:37:05 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 //Destructuring;
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./components/DirectoryComponent";
-import  { CAMPSITES } from './shared/campsites';
-import './App.css';
+import { CAMPSITES } from "./shared/campsites";
+import "./App.css";
 
 // import Component1 from "./components/Component1";
 class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
     };
+  }
 
-    render() {
-        return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
-                <Directory campsites = { this.state.campsites }/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">NuCamp</NavbarBrand>
+          </div>
+        </Navbar>
+
+        <Directory campsites={this.state.campsites} />
+      </div>
+    );
+  }
 }
 
 export default App;
