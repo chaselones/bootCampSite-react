d1497045df9ad887b6cad4c445bc84cdaecdce21
Chase Lones <chaselones@gmail.com>
2021-04-28 19:09:51 -0700

    React Redux Forms Revisited

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
-import Main from "./components/Main";
+import Main from "./components/MainComponent";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
 import "./App.css";
--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,6 +5,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 
 const maxLength = len => val => !(val) || (val.length <= len);
@@ -52,6 +53,7 @@ class CommentForm extends Component {
         this.state = {
             isModalOpen: false
         };
+
         this.toggleModal = this.toggleModal.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -132,6 +134,26 @@ class CommentForm extends Component {
     }
 }
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
