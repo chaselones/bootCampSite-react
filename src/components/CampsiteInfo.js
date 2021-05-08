4738ba1834fb005fd2374c0306ad1d6a4a95e0ca
Chase Lones <chaselones@gmail.com>
2021-05-08 11:11:50 -0700

    Update CampsiteInfo.js

--- src/components/CampsiteInfo.js
+++ src/components/CampsiteInfo.js
@@ -1,4 +1,4 @@
-import React , { Component } from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardImg,
@@ -19,20 +19,20 @@ const maxLength = (len) => (val) => !val || val.length <= len;
 const minLength = (len) => (val) => val && val.length >= len;
 
 class CommentForm extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isModalOpen: false,
-      
+
     };
     this.toggleModal = this.toggleModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-}
+  }
 
-handleSubmit(values) {
-  console.log("Current state is: " + JSON.stringify(values));
-  alert("Current state is: " + JSON.stringify(values));
-}
+  handleSubmit(values) {
+    console.log("Current state is: " + JSON.stringify(values));
+    alert("Current state is: " + JSON.stringify(values));
+  }
 
   toggleModal() {
     this.setState({
@@ -40,65 +40,65 @@ handleSubmit(values) {
     });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        <Button outline onClick={this.toggleModal}><i className="fa-lg fa fa-pencil"/>Submit Comment</Button>
+        <Button outline onClick={this.toggleModal}><i className="fa-lg fa fa-pencil" />Submit Comment</Button>
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
-            <ModalBody>
-              <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
               <Label htmlFor="rating" md={2}>
-                  Rating
+                Rating
                 </Label>
-                <Control.select model=".rating"
-                      name="rating"
-                      id="rating"
-                      className="form-control"
-                      defaultValue="1">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Control.select>
-                <Label htmlFor="author" md={12}>
-                  Your Name
+              <Control.select model=".rating"
+                name="rating"
+                id="rating"
+                className="form-control"
+                defaultValue="1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Control.select>
+              <Label htmlFor="author" md={12}>
+                Your Name
                 </Label>
-                <Control.text className="form-control" model=".author"
-                      id="author"
-                      name="author"
-                      placeholder="Your Name"
-                      validators={{
-                        minLength: minLength(2),
-                        maxLength: maxLength(15),
-                      }}/>
-                       <Errors
-                    className="text-danger"
-                    model=".author"
-                    show="touched"
-                    component="div"
-                    messages={{
-                      required: "Required",
-                      minLength: "Must be at least 2 characters",
-                      maxLength: "Must be 15 characters or less",
-                    }}
-                  />
-                      <Label htmlFor="text" md={2}>
-                  Comment
+              <Control.text className="form-control" model=".author"
+                id="author"
+                name="author"
+                placeholder="Your Name"
+                validators={{
+                  minLength: minLength(2),
+                  maxLength: maxLength(15),
+                }} />
+              <Errors
+                className="text-danger"
+                model=".author"
+                show="touched"
+                component="div"
+                messages={{
+                  required: "Required",
+                  minLength: "Must be at least 2 characters",
+                  maxLength: "Must be 15 characters or less",
+                }}
+              />
+              <Label htmlFor="text" md={2}>
+                Comment
                 </Label>
-                <Control.textarea
-                      model=".text"
-                      id="text"
-                      name="text"
-                      rows="12"
-                      className="form-control"
-                    ></Control.textarea>
-                    <Button type="submit" color="primary">
-                    Send Feedback
+              <Control.textarea
+                model=".text"
+                id="text"
+                name="text"
+                rows="12"
+                className="form-control"
+              ></Control.textarea>
+              <Button type="submit" color="primary">
+                Send Feedback
                   </Button>
-              </LocalForm>
-            </ModalBody>
+            </LocalForm>
+          </ModalBody>
         </Modal>
       </div>
     )
